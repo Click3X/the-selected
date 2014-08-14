bb07555bb61b5c7558e03bb51173d164a6847fbf
Charles Howard Davis III <charles@click3x.com>
2014-08-14 15:33:30 -0400

    Added Smooth Scroll

--- js/main.js
+++ js/main.js
@@ -6,23 +6,22 @@ jQuery(document).ready(function($) {
     $(outlines).appendTo('body');
     $(outlines).click(function () {
       $('*').toggleClass('outlines');
-    });
+     });
 
 
     // SMOOTH SCROLL
     $(function() {
        $('a[href*=#]:not([href=#])').click(function() {
          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-     var target = $(this.hash);
-     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-     if (target.length) {
-       $('html,body').animate({
-         scrollTop: target.offset().top
-       }, 1000);
-       return false;
-     }
-   }
- });
-	});
-
-});
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+           $('html,body').animate({
+            scrollTop: target.offset().top
+           }, 800);
+           return false;
+          }
+         }
+        });
+      });
+   });
